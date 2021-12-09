a725fb2632413aecad2c3d5176f0eaf3b14e27cc
Santiago Julio Camacho <tataojc@gmail.com>
2021-12-09 18:43:41 -0500

    Update for components

--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+    <App>
+        <NavBar>
+            <Header/>
+            <LoginW> 
+                <Login/> 
+                <Singup/> 
+            </LoginW>
+        </NavBar>
+    </App>,
+
+document.getElementById('app'));
 
